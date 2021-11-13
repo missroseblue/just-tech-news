400bbe461df16800eda63f895b41e892f39a4945
Sandra Caceres <missroseblue@gmail.com>
2021-11-13 13:13:30 -0800

    added login/logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,62 +1,51 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
 
-router.get("/", (req, res) => {
+// get all posts for homepage
+router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ["username"],
-        },
+          attributes: ['username']
+        }
       },
       {
         model: User,
-        attributes: ["username"],
-      },
-    ],
+        attributes: ['username']
+      }
+    ]
   })
-    .then((dbPostData) => {
-      // pass a single post object into the homepage template
+    .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      console.log(dbPostData[0]);
       res.render('homepage', { posts });
-
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get("/", (req, res) => {
-  res.render("homepage", {
-    id: 1,
-    post_url: "https://handlebarsjs.com/guide/",
-    title: "Handlebars Docs",
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: "test_user",
-    },
-  });
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
